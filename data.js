20196096f57d68754aada76f6c2abf40ee405fdf
bilal ahmed <ahmed.bilal7191@gmail.com>
2024-02-25 18:13:31 +0530

    final commit

---
+++ data.js
@@ -0,0 +1,113 @@
+let data = [
+
+    {
+        source: "Tere Vaaste Main Falak Se Chand Launga(PagalWorld.com.se).mp3",
+        name: "Tere waste-Zara Hatke Zara Bachke",
+        singer: "Arijit Sing"
+    },
+
+    {
+        source: "Tere Hawaale_64(PagalWorld.com.se).mp3",
+        name: "Tere Hawale-Laal Singh Chaddha",
+        singer: "Arijit"
+    },
+
+    {
+        source: "Tu Hai To Mujhe Phir Aur Kya Chahiye(PagalWorld.com.se).mp3",
+        name: "Tu Hai To Mujhe-Zara Hatke Zara Bachke",
+        singer: "Arijit"
+    },
+
+    {
+        source: "As-Long-As-You-Love-Me---Justin-Bieber(PagalWorld).mp3",
+        name: "As-Long-As-You - Believe",
+        singer: "Arijit"
+    },
+
+    {
+        source: "Anyone(PagalWorld.com.se).mp3",
+        name: "Anyone - Believe",
+        singer: "Justin Bieber"
+    },
+
+    {
+        source: "Zayn_-_Dusk_Till_Dawn_Ft_Sia_CeeNaija.com_.mp3",
+        name: "Dusk_Till_Dawn ",
+        singer: "Zayn Malik"
+    },
+
+    {
+        source: "Dame Tu Cosita(PagalWorld.com.se).mp3",
+        name: "Dame Tu Cosita ",
+        singer: "Cartoons"
+    },
+
+    {
+        source: "Ek-Bar-Hi-Kiya-Toh-Yaro-Pyar-Kya-Kiya(PagalWorldl).mp3",
+        name: "Ek-Bar-Hi-Kiya-Toh ",
+        singer: "Tu Jhooti Mein Makkar"
+    },
+
+    {
+        source: "Christina_Perri_-_A_Thousand_Years_(Jesusful.com).mp3",
+        name: "A_Thousand_Years",
+        singer: "Christina_Perri"
+    },
+
+    {
+        source: "Sweat Nobody Is Listening 128 Kbps.mp3",
+        name: "Sweat",
+        singer: "Zayn Malik"
+    }
+
+
+    // {source:"Tu Hai To Mujhe Phir Aur Kya Chahiye(PagalWorld.com.se).mp3",
+    // name:"Tu hai toh mujhe",
+    // singer:"Arijit"},
+
+    // {source:"Tu Hai To Mujhe Phir Aur Kya Chahiye(PagalWorld.com.se).mp3",
+    // name:"Tu hai toh mujhe",
+    // singer:"Arijit"},
+
+
+    // {source:"Tu Hai To Mujhe Phir Aur Kya Chahiye(PagalWorld.com.se).mp3",
+    // name:"Tu hai toh mujhe",
+    // singer:"Arijit"},
+
+
+    // {source:"Zayn_-_Dusk_Till_Dawn_Ft_Sia_CeeNaija.com_.mp3",
+    // name:"Zayn Malik",
+    // singer:"Zayn"},
+
+    // {source:"Zayn_-_Dusk_Till_Dawn_Ft_Sia_CeeNaija.com_.mp3",
+    // name:"Zayn Malik",
+    // singer:"Zayn"}
+
+
+]
+let x = Math.round(Math.random() * 10);
+console.log(x)
+// console.log(data[0].name,data[0].singer,data[0].source)
+// console.log(Object.keys(data[0]))
+
+for (let index = 0; index < data.length; index++) {
+    const finelement = data[index];
+    // console.log(finelement)
+    // console.log(Object.values(finelement)[0] + "\n" + Object.values(finelement)[2])
+    let musiclist1 = Object.values(finelement)[0] + "\n" + Object.values(finelement)[2]
+
+
+
+
+}
+
+let musiclist = Object.values(data[0])[1] + "\n" + Object.values(data[0])[2]
+    // console.log(musiclist)
+// console.log("naaam souceka" + data[0])
+
+
+
+
+
+
+
